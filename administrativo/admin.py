e9af0179812235d0121c183883b1d21f20cd9f94
Carlos Morales <morales_carl@outlook.com>
2025-01-16 23:10:54 -0500

    Actualización clases para admin.py

--- administrativo/admin.py
+++ administrativo/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 
 # Register your models here.
 
-from administrativo.models import Estudiante
+from administrativo.models import Estudiante, NumeroTelefonico
 
-admin.site.register(Estudiante)
+#admin.site.register(Estudiante)
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ('nombre','apellido','cedula')
+    search_fields = ('nombre','cedula')
+
+admin.site.register(Estudiante,EstudianteAdmin)
+
+#admin.site.register(NumeroTelefonico)
+
+class NumeroTelefonoAdmin(admin.ModelAdmin):
+    list_display = ('telefono','tipo','get_estudiante')
+    raw_id_fields = ('estudiante',)
+    def get_estudiante(self, obj):
+        """ """
+        return obj.estudiante.apellido
+    
+admin.site.register(NumeroTelefonico,NumeroTelefonoAdmin)
